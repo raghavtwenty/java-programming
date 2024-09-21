248e27c82fd7b809aadf1ff1cf27b46d6b48dcf7
raghavtwenty <raghavtwenty@gmail.com>
2024-09-21 12:37:13 +0530

    added hello world

---
+++ code/_0001_hello_world.java
@@ -0,0 +1,25 @@
+/*
+Filename: _0001_hello_world.java
+Title: First program in java
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: September 21, 2024 | Last Updated: September 21, 2024
+Language: Java
+*/
+
+// This is the declaration of the class named HelloWorld
+// In Java, everything resides inside a class
+public class HelloWorld {
+
+    // The main method is the entry point of any Java application
+    // It's where the program starts executing
+    public static void main(String[] args) {
+        
+        // System.out.println is used to print text to the console
+        // The string "Hello, World!" will be displayed when the program runs
+        System.out.println("Hello, World!");
+
+        // End of the main method
+    }
+
+    // End of the HelloWorld class
+}
