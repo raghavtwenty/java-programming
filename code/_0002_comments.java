5f43427924e4393611d0ba444639ab5e403a62a7
raghavtwenty <raghavtwenty@gmail.com>
2024-09-21 12:37:26 +0530

    added comments

---
+++ code/_0002_comments.java
@@ -0,0 +1,29 @@
+/*
+Filename: _0002_comments.java
+Title: comments in java
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: September 21, 2024 | Last Updated: September 21, 2024
+Language: Java
+*/
+
+/*
+Multi-line comment with developer details on the top of the program
+is called docstring
+*/
+
+// Required imports - Single line comment starts with //
+import java.util.*;
+
+// Main class definition
+public class CommentsExample {
+
+    // Main function
+    public static void main(String[] args) {
+        /*
+        Multi-line comment starts and ends with
+        */
+        System.out.println("Hello, World!");
+        // Return statement not needed in void functions in Java
+    } 
+}
+
