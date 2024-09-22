3d7f4e71d874dc85104b9a0e2597ebd4a79239e6
raghavtwenty <raghavtwenty@gmail.com>
2024-09-22 15:30:03 +0530

    added arithmetic operators

---
+++ code/_0006_arithmetic.java
@@ -0,0 +1,34 @@
+/*
+Filename: _0006_arithmetic.java
+Title: Arithmetic operators
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: September 22, 2024 | Last Updated: September 22, 2024
+Language: Java 
+*/
+
+public class _0006_arithmetic {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        int result;
+        
+        // Addition
+        result = a + b;
+        System.out.println("Addition: " + a + " + " + b + " = " + result);
+        
+        // Subtraction
+        result = a - b;
+        System.out.println("Subtraction: " + a + " - " + b + " = " + result);
+        
+        // Multiplication
+        result = a * b;
+        System.out.println("Multiplication: " + a + " * " + b + " = " + result);
+        
+        // Division
+        result = a / b;
+        System.out.println("Division: " + a + " / " + b + " = " + result);
+        
+        // Modulus
+        result = a % b;
+        System.out.println("Modulus: " + a + " % " + b + " = " + result);
+    }
+}
