e7fcefe3a9b47b3991902ad62e00e7a2e541bbdf
raghavtwenty <126254197+raghavtwenty@users.noreply.github.com>
2024-10-15 22:15:32 +0530

    Added _0007_comparision_operators.java

---
+++ code/_0007_comparision_operators.java
@@ -0,0 +1,104 @@
+/*
+Filename: _0007_comparision_operators.java
+Title: Comparison operators
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: October 15, 2024 | Last Updated: October 15, 2024
+Language: Java 
+*/
+
+// Main class
+public class ComparisonOperators {
+
+    // Main method
+    public static void main(String[] args) {
+        // Variables
+        int a = 10, b = 5;
+
+        // Equal to (==)
+        if (a == b) {
+            System.out.println("a is equal to b");
+        } else {
+            System.out.println("a is not equal to b");
+        }
+
+        // Not equal to (!=)
+        if (a != b) {
+            System.out.println("a is not equal to b");
+        } else {
+            System.out.println("a is equal to b");
+        }
+
+        // Greater than (>)
+        if (a > b) {
+            System.out.println("a is greater than b");
+        } else {
+            System.out.println("a is not greater than b");
+        }
+
+        // Less than (<)
+        if (a < b) {
+            System.out.println("a is less than b");
+        } else {
+            System.out.println("a is not less than b");
+        }
+
+        // Greater than or equal to (>=)
+        if (a >= b) {
+            System.out.println("a is greater than or equal to b");
+        } else {
+            System.out.println("a is not greater than or equal to b");
+        }
+
+        // Less than or equal to (<=)
+        if (a <= b) {
+            System.out.println("a is less than or equal to b");
+        } else {
+            System.out.println("a is not less than or equal to b");
+        }
+
+        // Demonstrating with floating-point numbers
+        float x = 10.5f, y = 5.5f;
+
+        // Equal to (==)
+        if (x == y) {
+            System.out.println("x is equal to y");
+        } else {
+            System.out.println("x is not equal to y");
+        }
+
+        // Not equal to (!=)
+        if (x != y) {
+            System.out.println("x is not equal to y");
+        } else {
+            System.out.println("x is equal to y");
+        }
+
+        // Greater than (>)
+        if (x > y) {
+            System.out.println("x is greater than y");
+        } else {
+            System.out.println("x is not greater than y");
+        }
+
+        // Less than (<)
+        if (x < y) {
+            System.out.println("x is less than y");
+        } else {
+            System.out.println("x is not less than y");
+        }
+
+        // Greater than or equal to (>=)
+        if (x >= y) {
+            System.out.println("x is greater than or equal to y");
+        } else {
+            System.out.println("x is not greater than or equal to y");
+        }
+
+        // Less than or equal to (<=)
+        if (x <= y) {
+            System.out.println("x is less than or equal to y");
+        } else {
+            System.out.println("x is not less than or equal to y");
+        }
+    }
+}
