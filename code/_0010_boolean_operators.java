0e84fef36fcd137862365461be9ebca60647e377
raghavtwenty <126254197+raghavtwenty@users.noreply.github.com>
2024-10-16 20:34:40 +0530

    Added _0010_boolean_operators.java

---
+++ code/_0010_boolean_operators.java
@@ -0,0 +1,27 @@
+/*
+Filename: _0010_boolean_operators.java
+Title: Boolean operators
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: October 16, 2024 | Last Updated: October 16, 2024
+Language: Java 
+*/
+
+public class BooleanOperators {
+    public static void main(String[] args) {
+        // Using boolean types in Java
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        if (isTrue) {
+            System.out.println("isTrue is true");
+        } else {
+            System.out.println("isTrue is false");
+        }
+
+        if (isFalse) {
+            System.out.println("isFalse is true");
+        } else {
+            System.out.println("isFalse is false");
+        }
+    }
+}
