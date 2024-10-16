eb56caa6c11b624fc0200e97d62d7f1653fb386c
raghavtwenty <126254197+raghavtwenty@users.noreply.github.com>
2024-10-16 20:38:10 +0530

    Added _0011_if_condition.java

---
+++ code/_0011_if_condition.java
@@ -0,0 +1,19 @@
+/*
+Filename: _0011_if_condition.java
+Title: If condition
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: October 16, 2024 | Last Updated: October 16, 2024
+Language: Java | Version: OpenJDK 17
+*/
+
+public class IfCondition {
+    public static void main(String[] args) {
+        int num = 10;
+
+        // If statement
+        if (num > 0) {
+            // If true, executes
+            System.out.println("The number is positive.");
+        }
+    }
+}
