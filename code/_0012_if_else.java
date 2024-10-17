ceb29b2b4b4d2504060235e8ae7a9021e617be22
raghavtwenty <126254197+raghavtwenty@users.noreply.github.com>
2024-10-17 21:03:03 +0530

    Added _0012_if_else.java

---
+++ code/_0012_if_else.java
@@ -0,0 +1,21 @@
+/*
+Filename: _0012_if_else.java
+Title: If-Else Condition
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: October 17, 2024 | Last Updated: October 17, 2024
+Language: Java 
+*/
+
+public class IfElseCondition {
+    public static void main(String[] args) {
+        int num = -10;
+
+        // If statement true
+        if (num > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            // Else executes if the above condition is false
+            System.out.println("The number is non-positive.");
+        }
+    }
+}
