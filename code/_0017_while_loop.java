7ebd7c5c6f9f6b5528158c85db39657104f83dc7
raghavtwenty <126254197+raghavtwenty@users.noreply.github.com>
2024-10-18 21:23:45 +0530

    Added _0017_while_loop.java

---
+++ code/_0017_while_loop.java
@@ -0,0 +1,22 @@
+/*
+Filename: _0017_while_loop.java
+Title: While Loop
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: October 18, 2024 | Last Updated: October 18, 2024
+Language: Java 
+*/
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        // Variable initialization
+        int count = 1;
+
+        // while loop
+        while (count <= 5) {
+            // until the condition is true the block executes
+            System.out.println("Count: " + count);
+            count++; // increment count
+        }
+    }
+}
