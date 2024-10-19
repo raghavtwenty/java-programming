f803ee6444ca466888ef70b185b0db7c775ba954
raghavtwenty <126254197+raghavtwenty@users.noreply.github.com>
2024-10-19 21:32:34 +0530

    Added  _0018_for_loop.java

---
+++ code/_0018_for_loop.java
@@ -0,0 +1,17 @@
+/*
+Filename: _0018_for_loop.java
+Title: for loop
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: October 19, 2024 | Last Updated: October 19, 2024
+Language: Java 
+*/
+
+public class ForLoop {
+    // Main method
+    public static void main(String[] args) {
+        // for loop - initial value, stop value, step value
+        for (int count = 1; count <= 5; count++) {
+            System.out.println("Count: " + count);
+        }
+    }
+}
