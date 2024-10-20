935e08546ecd0dc630b6b4b87811e4a6580f2238
raghavtwenty <126254197+raghavtwenty@users.noreply.github.com>
2024-10-20 21:41:33 +0530

    Added _0023_function_calls.java

---
+++ code/_0023_function_calls.java
@@ -0,0 +1,21 @@
+/*
+Filename: _0023_function_calls.java
+Title: Function Calls
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: October 20, 2024 | Last Updated: October 20, 2024
+Language: Java 
+*/
+
+public class FunctionCalls {
+
+    // Main method
+    public static void main(String[] args) {
+        // Function call
+        greet();
+    }
+
+    // Function declaration and body
+    public static void greet() {
+        System.out.println("Hello, World!");
+    }
+}
