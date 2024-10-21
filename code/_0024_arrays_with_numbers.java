cc4cdba60ddf6a887f2975e742e4b8186f644f96
raghavtwenty <126254197+raghavtwenty@users.noreply.github.com>
2024-10-21 20:51:51 +0530

    Added _0024_arrays_with_numbers.java

---
+++ code/_0024_arrays_with_numbers.java
@@ -0,0 +1,22 @@
+/*
+Filename: _0024_arrays_with_numbers.java
+Title: Arrays with numbers in Java
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: October 21, 2024 | Last Updated: October 21, 2024
+Language: Java
+*/
+
+// Main class
+public class ArraysWithNumbers {
+
+    // Main method
+    public static void main(String[] args) {
+        // Declaration and initialization of array
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        // Accessing array elements
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("numbers[" + i + "] = " + numbers[i]);
+        }
+    }
+}
